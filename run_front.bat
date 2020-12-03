fdf2e86f31db0e19fa1ec1dbaab7ec0bac55156d
Blue&White <terry25031069@gmail.com>
2020-12-03 17:00:16 +0800

    run_front.bat information edited

--- run_front.bat
+++ run_front.bat
@@ -1,3 +1,3 @@
 @echo off
 start cmd /k "cd C:\Users\nsl814888\Desktop\Network-Infomation-Web&&http-server -c-1"
-start cmd /k "python C:\Users\nsl814888\Desktop\Network-Infomation-Web\fsiem_rest_api\account\fff.py"
+start cmd /k "python C:\Users\nsl814888\Desktop\Network-Information-Web\fsiem_rest_api\account\fff.py"
