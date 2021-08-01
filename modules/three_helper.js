e7ed7f8110b988389efe0bf96418a5851edc7bc3
Blue&White <terry25031069@gmail.com>
2021-08-02 00:48:21 +0800

    delete temp file

--- modules/three_helper.js
+++ modules/three_helper.js
@@ -1,5 +1,7 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
 export { MAX_POINTS, drawCount, createPlane, createBuilding, getRandomColor };
+
+
 let MAX_POINTS = 200;
 let drawCount;
 //建立平面
@@ -28,17 +30,42 @@ function createBuilding(building, group)
   for(let i =0;i < floor; i++)
   {
     var geometry = new THREE.BoxGeometry(width, height / floor, depth)
-    //更改成如果遇到世界地圖牆壁，就改成 BasicMaterial，也就是正常不透明的材質
-    var material = (width == 3 && height == 3 && depth ==3 ? 
-                    new THREE.MeshToonMaterial({color: c, opacity : 0.7 ,transparent : true}) :
-                    new THREE.MeshLambertMaterial({color: c, opacity : 0.8, transparent : true}));
+    var material
+    //更改成如果遇到世界地圖的方格，就改成 BasicMaterial，也就是正常不透明的材質
+    if(width == 3 && height == 3 && depth ==3){
+        material = new THREE.MeshToonMaterial({color: c, opacity : 0.7, transparent : true})
+    }else if(i == floor - 1){
+        var canvas = document.createElement("canvas")
+        canvas.width = width * 20
+        canvas.height = depth * 20
+        var context = canvas.getContext("2d");
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.fillStyle = "#" + c.toString(16);
+        context.fillRect(0, 0, width * 20, depth * 20);
+        context.fillStyle = "#000000";
+        context.font = "50pt Arial";
+        context.fillText(building.title, canvas.width / 2, canvas.height / 2);
+
+        material = []
+        for(var ii = 0; ii < geometry.faces.length; ii++){
+          var texture = new THREE.Texture(canvas);
+          texture.needsUpdate = true;
+          if(ii == 2) material.push(new THREE.MeshBasicMaterial({color: c, map: texture}))
+          else material.push(new THREE.MeshLambertMaterial({color: c, opacity : 0.8, transparent : true}))
+        }
+    }else{
+        material = new THREE.MeshLambertMaterial({color: c, opacity : 0.8, transparent : true})
+    }
     var Mesh = new THREE.Mesh(geometry, material);
     Mesh.name = building.id + "_" + (i + 1).toString();
     Mesh.position.set(x, y + y_shift + i * height / floor - floor + 1, z );
     Mesh.castShadow = true;
     Mesh.receiveShadow = false;
     //console.log("BoxGeometry" + Mesh.name + "Added");
+
     group.add(Mesh);
+
   }
 }
 
